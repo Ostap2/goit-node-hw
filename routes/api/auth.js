ac6b313907103b9af55588c23bf8a7e2ee821d45
Ostap <Ostapkriven13@gmail.com>
2023-12-30 14:43:08 +0200

    Done

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,11 +1,13 @@
 const express = require("express");
-
 const controller = require("../../controller/auth");
-
-const router = express.Router();
-
 const { validateBody, authenticate } = require("../../middlewares");
 const { schemas } = require("../../models/user");
+const { controllerWrapper } = require("../../erorr");
+const multer = require("multer");
+
+const upload = multer({ dest: "tmp/" });
+
+const router = express.Router();
 
 router.post("/register", validateBody(schemas.registerSchema), controller.register);
 
@@ -14,5 +16,12 @@ router.post("/login", validateBody(schemas.loginSchema), controller.login);
 router.get("/current", authenticate, controller.current);
 
 router.post("/logout", authenticate, controller.logout);
+
+router.patch(
+  "/avatars",
+  authenticate,
+  upload.single("avatar"),
+  controller.updateAvatar
+);
 
 module.exports = router;
